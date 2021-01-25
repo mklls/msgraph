de0ee7ef881efb9feb8befbf7bbafdc2e42da15b
mklls <pzju@outlook.com>
2021-01-25 17:37:32 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,167 @@
+const path = require('path');
+const axios = require('axios');
+const chalk = require('chalk');
+const auth = require(path.join(__dirname, '/lib/auth'));
+
+require('dotenv-safe').config();  // load environment variable
+
+function log() {
+    let args = Array.prototype.slice.call(arguments);
+    args.unshift(Date());
+    console.log.apply(console, args);
+}
+
+// common APIs
+const common = [
+    '/groups',
+    '/domains',
+    '/sites/root/drives',
+    '/sites?search=123',
+    '/sites/root/sites',
+    '/sites/root/contentTypes',
+    '/sites/root/lists',
+    '/users/delta?$select=displayName,givenName,surname',
+    '/applications?$count=true'
+];
+
+// APIs which need post/get with extra data
+const REST = [
+    '/',
+    '/messages',
+    '/events',
+    '/drive/root',
+    '/calendars',
+    '/licenseDetails',
+    '/drive/root/children',
+    '/oauth2PermissionGrants',
+    '/events?$select=attendees,start,end,location'
+];
+
+async function batching(batching, code) {
+    for (let r of batching) {
+        try {
+            let res = await axios.post('/$batch', r.batch);
+            for (let i of res.data.responses) {
+                if (i.status != code) {
+                    log(chalk.bold(r.user), chalk.redBright('error'), i.body.error.message);
+                }
+            }
+            log(chalk.bold(r.user), chalk.blueBright('done'));
+        } catch (error) {
+            log(chalk.bold(r.user), chalk.redBright(error.message));
+        }
+    }
+    return true;
+}
+
+function init() {
+    return new Promise (function (resolve, reject) {
+        auth.getApplicationToken().then((token) => {
+            axios.defaults.baseURL = 'https://graph.microsoft.com/v1.0';
+            axios.defaults.headers = {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+                'User-Agent': 'Puppy/0.1',
+            };
+            resolve();
+        }, reason => reject(reason));
+    });
+}
+
+function getUsers() {
+    console.log(chalk.cyanBright('-----------------------------------'));
+    return new Promise(function (resolve, reject) {
+        axios
+            .get('/users')
+            .then((res) => resolve(res.data.value.map((v) => v.id)))
+            .catch(error => reject(error));
+    });
+}
+
+async function active(users) {
+
+    let jobs = users.map(v => {
+        let batch = { requests: [] };
+
+        for (let i = 0; i < REST.length; i++) {
+            batch.requests.push({
+                id: i,
+                method: 'GET',
+                url: `/users/${v}${REST[i]}`,
+            });
+        }
+        return {user: v, batch: batch};
+    });
+
+    log(chalk.blue('Application information'));
+    await batching([{
+        user: 'staff@xxx.cf', // any string in order to keep with api standard
+        batch: {
+            requests: common.map((v, i) => {
+                return {
+                    id: i,
+                    method: 'GET',
+                    url: v,
+                };
+            }),
+        },
+    }], 200);
+
+    log(chalk.blue('profile'));
+    await batching(jobs, 200);
+
+    return users;
+}
+
+async function getMessages(users) {
+    let jobs = []
+    log(chalk.blue('fetch messages'));
+    let us = await axios.get('/users');
+    us = us.data.value.map((v) => v.mail);
+    let filter = '$filter=';
+
+    for (let i = 0; i < us.length; i++) {
+        filter += `from/emailAddress/address eq '${us[i]}' `;
+        
+        if (i != us.length-1) filter += 'or ';
+    }
+
+    for (let u of users) {
+        let res = await axios.get(`/users/${u}/mailFolders/inbox/messages?` + filter);
+        let batch = { requests: [] };
+        let temp = Array.from(res.data.value, (v) => v.id);
+            temp = temp.slice(0, 4);
+        if (temp.length === 0) continue;
+
+        for (let i = 0; i < temp.length; i++) {
+            batch.requests.push({
+                id: i,
+                method: 'DELETE',
+                url: `/users/${u}/messages/${temp[i]}`
+            });
+        }
+
+        jobs.push({
+            user: u, 
+            batch: batch 
+        });
+    }
+    return jobs;
+}
+// TODO 
+// read messages from inbox
+async function readMessages(){}
+
+async function deleteMessages(jobs) {
+    log(chalk.blue('removing messages'))
+    await batching(jobs, 204);
+    console.log(chalk.yellowBright('-----------------------------------'));
+}
+
+
+init()
+    .then(getUsers)
+    .then(active)
+    .then(getMessages)
+    .then(deleteMessages)
+
---
+++ mail.js
@@ -0,0 +1,141 @@
+const axios = require('axios');
+const path = require('path');
+const util = require(path.join(__dirname, 'lib/auth'));
+const faker = require('faker');
+const fs = require('fs');
+const chalk = require('chalk');
+
+require('dotenv-safe').config();
+faker.locale = 'zh_CN'; //en_US
+
+const quote = fs
+    .readFileSync(path.join(__dirname, '/assets/quote.txt'))
+    .toString()
+    .split('\n');
+const characters = fs
+    .readFileSync(path.join(__dirname, './assets/hans.txt'))
+    .toString()
+    .split('\n');
+const skills = fs
+    .readFileSync(path.join(__dirname, './assets/skill.txt'))
+    .toString()
+    .split('\n');
+
+const randomName = () => faker.name.lastName() + faker.name.firstName();
+const randomQuote = () => quote[parseInt(Math.random() * quote.length - 1, 10)];
+const randomSkill = () => {
+    let suffix = skills[parseInt(Math.random()*skills.length, 10) - 1];
+    let length = parseInt(Math.random() * 3 + 1, 10);
+    let temp = ''
+    for (let i = 0; i < length; i++) {
+        temp += characters[parseInt(Math.random() * characters.length, 10) - 1];
+    }
+
+    return temp + suffix;
+}
+
+function log() {
+    let args = Array.prototype.slice.call(arguments);
+    args.unshift(Date());
+    console.log.apply(console, args);
+}
+
+function init() {
+    return new Promise(function (resolve, reject) {
+        util.getApplicationToken().then((token) => {
+            axios.defaults.baseURL = 'https://graph.microsoft.com/v1.0';
+            axios.defaults.headers = {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+                'User-Agent': 'Puppy/0.2',
+            };
+            resolve();
+        }, reason => reject(reason));
+    });
+}
+
+
+function getUsers() {
+    return new Promise(function (resolve, reject) {
+        axios
+            .get('/users')
+            .then((res) => resolve(res.data.value.map((v) => v.mail)))
+            .catch((error) => reject(error));
+    });
+}
+
+function generateMessage(to) {
+    let mail = {
+        message: {
+            subject: 'Title',
+            body: { contentType: 'HTML', content: 'article' },
+            toRecipients: [ { emailAddress: { address: '' } } ],
+        },
+        saveToSentItems: 'false',
+    };
+
+    mail.message.subject =  randomName() + ' ' + 
+        faker.hacker.verb() + ' ' +
+        randomSkill();
+
+    mail.message.body.content = '' +
+        '<p><strong>' + randomQuote() + '</strong></p>' +
+        '<strong>' + Date() + '</strong>' + 
+        '<br>' +
+        '<img src="https://source.unsplash.com/random/200x200" alt="Wish I were there...">' +
+        '<p>' + faker.lorem.paragraphs(3, '<br>') + '</p>' + 
+        '<br>' +
+        '<strong>' + randomName()  + '</strong>'+ 
+        '在' + faker.address.city() + '找到' + 
+        '<strong>' + randomSkill() + '</strong>';
+
+    mail.message.toRecipients[0].emailAddress.address = to;
+
+    return mail;
+}
+
+async function sendMail(users) {
+
+    async function broadcast(from) {
+        for (let to of users) {
+            await axios.post(
+                `/users/${from}/sendMail`,
+                generateMessage(to)
+            );
+        }
+        return true;
+    }
+
+    // node mail.js [ANY STUFF] will execute this function instead of broadcast
+    async function me() {
+        let u = users[Math.floor(Math.random()*users.length)];
+        let master = process.env.MAIL
+        
+        await axios.post(`/users/${u}/sendMail`,generateMessage(master));
+        return u;
+    }
+
+    if (process.argv.length > 2) {
+        try {
+            let u = await me();
+            log(chalk.greenBright(u), 'done');
+        } catch (error) {
+            log(chalk.redBright(error.message));
+        }
+    } else {
+        for (let i of users) {
+            try {
+                await broadcast(i);
+                log(chalk.greenBright(i), 'done');
+            } catch (error) {
+                log(chalk.redBright(error.message));
+            }
+        }
+    }
+
+    return true
+}
+
+init()
+    .then(getUsers)
+    .then(sendMail);
