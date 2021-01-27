d89a7785db9305b002cc13bdeab66ac2cfb44d39
mklls <pzju@outlook.com>
2021-01-27 15:32:48 +0800

    test

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 white=$'\e[0m'
 
-read -p $cyn"tenant id: "$white tenant_id
-read -p $cyn"client id: "$white client_id
-read -p $cyn"client secret: "$white client_secret
-read -p $cyn"email: "$white email
+read -p "${cyn}tenant id:${white}" tenant_id
+read -p "${cyn}client id:${white}" client_id
+read -p "${cyn}client secret:${white}" client_secret
+read -p "${cyn}email:${white}" email
 
 mgh_has() {
     # 2>&1 redirect stderr to stdout
@@ -25,7 +25,7 @@ install() {
     if mgh_has $1; then
         echo "[$1] installed"
     else
-        echo &>2 "$red[error]$white failed to install $1"
+        echo &>2 "${red}[error]${white} failed to install $1"
         exit 1
     fi
 }
@@ -40,30 +40,30 @@ install_node_via_nvm() {
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     
     if nvm install --lts && mgh_has "node" && mgh_has "npm"; then
-        echo "$grn[node]$white installed"
+        echo "${grn}[node]${white} installed"
         echo "installing yarn"
         npm i -g yarn
 
         if mgh_has "yarn"; then
-            echo "$grn[yarn]$white installed"
+            echo "${grn}[yarn]${white} installed"
         else 
-            echo >&2 "$red[error]$white failed to install yarn"
+            echo >&2 "${red}[error]${white} failed to install yarn"
         fi
     else
-        echo >&2 "$red[error]$white failed to install node"
+        echo >&2 "${red}[error]${white} failed to install node"
         exit 1
     fi
 }
 
 install_prerequisites() {
     if mgh_has "git"; then
-        echo "$grn[git]$white is already installed"
+        echo "${grn}[git]${white} is already installed"
     else
         install "git"
     fi
 
     if mgh_has "node" && mgh_has "npm"; then
-        echo "$grn[node npm]$white is already installed"
+        echo "${grn}[node npm]${white} is already installed"
     else
         install_node_via_nvm
     fi
@@ -74,7 +74,7 @@ install_prerequisites
 if git clone https://github.com/mklls/msgraph.git; then
     cd msgraph && cp .env.example .env
 else 
-    echo >&2 "$red[error]$white failed to clone repo" 
+    echo >&2 "${red}[error]${white} failed to clone repo" 
     exit 1
 fi
 
@@ -93,18 +93,18 @@ else
     npm install
 fi
 
-echo "$blu[info]$white scheduling tasks with cron"
+echo "${blu}[info]${white} scheduling tasks with cron"
 crontab -l | { cat; cat schedule; } | sed 's/no crontab for root//' | crontab -
 
 if [ $? -eq 0 ]; then
     node app/mail.js testing
 
     if [ $? -eq 0 ]; then
-        echo "$blu[info]$white testing passed"
-        echo "$blu[info]$white an email has been sent, please check your inbox"
+        echo "${blu}[info]${white} testing passed"
+        echo "${blu}[info]${white} an email has been sent, please check your inbox"
     else
-        echo "$red[error]$white testing failed" 
-        echo "you can edit in $mag.env$white file, please review the value to ensure that All of the information is corret"
+        echo "${red}[error]${white} testing failed" 
+        echo "you can edit in ${mag}.env${white} file, please review the value to ensure that All of the information is corret"
         echo "perhaps you need to add additional permissions in order to use"
         echo "bye"
     fi
@@ -113,13 +113,13 @@ if [ $? -eq 0 ]; then
         "file to have it automatically sourced upon login: (you may have to add" \
         "to more than one of the above files)"
 
-    echo "$cyn---------------------------$white"
+    echo "${cyn}---------------------------${white}"
     echo "export NVM_DIR=\"\$HOME/.nvm\""
     echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm"
     echo "[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion"
-    echo "$cyn---------------------------$white"
+    echo "${cyn}---------------------------${white}"
     exit 0
 else
-    echo >&2 "$red[error]$white failed to add task to cron"
+    echo >&2 "${red}[error]${white} failed to add task to cron"
     exit 1
 fi
